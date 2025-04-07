faa352361843958bf5de5bf4f86d27fd9f3d1b81
Blaxer <rayan.chaou@epita.fr>
2025-04-07 10:47:06 +0200

    init files

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { MoviesService } from '../services/movies.service';
+import { Observable } from 'rxjs';
+import { Movie } from '../models/movie';
+import { AsyncPipe } from '@angular/common';
+import { MovieComponent } from "./movie/movie.component";
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    MovieComponent
+],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  private readonly moviesService = inject(MoviesService)
+
+  movies$: Observable<Movie[]> = this.moviesService.getMovies()
+}
