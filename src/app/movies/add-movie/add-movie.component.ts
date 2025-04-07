4582cdf0379d2e8e34e50c88ff6ea17413047881
mathilde.copin <mathilde.copin@epita.fr>
2025-04-07 12:50:24 +0200

    feat(forms): add error message

--- src/app/movies/add-movie/add-movie.component.ts
+++ src/app/movies/add-movie/add-movie.component.ts
@@ -1,46 +1,3 @@
-// import { Component, inject } from '@angular/core';
-// import { Movie } from '../../models/movie';
-// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-// import { MoviesService } from '../../services/movies.service';
-// import { Router } from '@angular/router';
-// import { ToastrService } from 'ngx-toastr';
-//
-// @Component({
-//   selector: 'app-add-movie',
-//   standalone: true,
-//   imports: [FormsModule, ReactiveFormsModule],
-//   templateUrl: './add-movie.component.html',
-//   styleUrl: './add-movie.component.scss'
-// })
-// export class AddMovieComponent {
-//   private readonly moviesService = inject(MoviesService)
-//   private readonly router = inject(Router)
-//   private readonly toaster = inject(ToastrService)
-//
-//   movie: Movie = {
-//     title: '',
-//     director: '',
-//     releaseDate: new Date(),
-//     synopsis: '',
-//     id: undefined,
-//     rate: undefined,
-//     image: undefined
-//   }
-//
-//   addMovie(): void {
-//     this.moviesService.addMovie(this.movie).subscribe(
-//       () => {
-//         this.router.navigate(['/movies'])
-//         this.toaster.success('Le film a été ajouté avec succès', 'Succès', {
-//           timeOut: 3000,
-//           progressBar: true,
-//           progressAnimation: 'increasing'
-//         });
-//       }
-//     );
-//   }
-// }
-
 import {Component, inject} from '@angular/core';
 import {Movie} from '../../models/movie';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
@@ -48,11 +5,12 @@ import {MoviesService} from '../../services/movies.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
 import {directorValidator, releaseDateValidator, titleValidator} from "../movie-validators";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-add-movie',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './add-movie.component.html',
   styleUrl: './add-movie.component.scss'
 })
@@ -69,7 +27,7 @@ export class AddMovieComponent {
       title: new FormControl('', [Validators.required, titleValidator()]),
       director: new FormControl('', [Validators.required, directorValidator()]),
       releaseDate: new FormControl(new Date(), [Validators.required, releaseDateValidator()]),
-      synopsis: new FormControl('', [Validators.required, Validators.minLength(30)]),
+      synopsis: new FormControl('', [Validators.minLength(30)]),
     }
   );
 
