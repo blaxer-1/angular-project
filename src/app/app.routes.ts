5a4a4f3f9ce70e707aa914f38534cd002c25c7b3
Blaxer <rayan.chaou@epita.fr>
2025-04-07 13:02:26 +0200

    feat(inscription/login/account): add files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,21 @@ import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
 import { AddMovieComponent } from './movies/add-movie/add-movie.component';
 import { UpdateMovieComponent } from './movies/update-movie/update-movie.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
+import { AccountComponent } from './user/account/account.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { AddReviewComponent } from './reviews/add-review/add-review.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
-    { path: 'movies', component: MoviesComponent},
-    { path: 'add-movie', component: AddMovieComponent},
-    { path: 'add-movie/:id', component: UpdateMovieComponent},
+    { path: '', component: HomeComponent },
+    { path: 'movies', component: MoviesComponent },
+    { path: 'add-movie', component: AddMovieComponent },
+    { path: 'add-movie/:id', component: UpdateMovieComponent },
+    { path: "register", component: RegisterComponent },
+    { path: 'register/:id', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'account/:id', component: AccountComponent},
+    { path: 'reviews/:id', component: ReviewsComponent },
+    { path: 'add-review/:id', component: AddReviewComponent },
 ];
